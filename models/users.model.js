a53144f472199aba04923193feb641f86f87e2da
hairflicks <bweidner1998@gmail.com>
2023-03-29 11:06:03 +0100

    removed console.log

--- models/users.model.js
+++ models/users.model.js
@@ -3,7 +3,6 @@ const db = require('../db/connection')
 function fetchAllUsers() {
     return db.query(`SELECT * FROM users`)
     .then(({rows}) => {
-        console.log(rows)
         return rows
     })
 }
