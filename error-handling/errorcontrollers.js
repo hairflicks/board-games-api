22c7639f667466675c30a31a46986971fdddf54b
Hairflicks <bweidner1998@gmail.com>
2023-03-31 01:38:08 +0100

    completed ticket-20 paginating /api/reviews

--- error-handling/errorcontrollers.js
+++ error-handling/errorcontrollers.js
@@ -2,6 +2,7 @@ const express = require('express')
 
 
 function customErrors(err,req,res,next) {
+    console.log(err)
     if (err.status && err.msg) {
 
     res.status(err.status).send({msg: err.msg})
